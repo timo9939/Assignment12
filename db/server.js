a628d13009ab9b25fcd7f8fa4d7ac98c9634bb95
timo9939 <timothylau20199@gmail.com>
2022-12-19 18:17:49 +0000

    1219 push

--- db/server.js
+++ db/server.js
@@ -11,7 +11,7 @@ const db=mysql.createConnection({
 
     host:'localhost',
     user:'root',
-    password:'',
+    password:'password',
     database:'employee_db'
 })
 
--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// const mysql =require('mysql2');
-// const express=require('express')
-// const inquirer=require('inquirer')
-
-// const port=process.env.port || 3001;
-// const app=express();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// const db=mysql.createConnection({
-
-//     host:'localhost',
-//     user:'root',
-//     password:'',
-//     database:'employee_db'
-// })
 const inquirer=require('inquirer');
 const db = require('./db/server');
 
 
 let mainPrompt=()=>{
-return inquirer.prompt([{
+ inquirer.prompt([{
         type:'list',
         name:'firstPrompt',
         message:'Choose your action',
