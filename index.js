eb2fb7694d4f512ed25b666b07e6d9d342899c8d
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-19 21:51:57 +0000

    Huzzah

--- index.js
+++ index.js
@@ -3,70 +3,70 @@ const db = require('./db/server');
 
 
 let mainPrompt = async () => {
-    inquirer.prompt([{
+    const ans = await inquirer.prompt([{
         type: 'list',
         name: 'firstPrompt',
         message: 'Choose your action',
         choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
 
     }])
-        .then((ans) => {
-            if (ans.firstPrompt === 'view all departments') {
-                console.log('You choose ' + ans.firstPrompt)
-                // db.query(`SELECT * FROM department`,(err,output)=>{
+    if (ans.firstPrompt === 'view all departments') {
+        console.log('You choose ' + ans.firstPrompt)
+        // db.query(`SELECT * FROM department`,(err,output)=>{
 
-                // if(err) throw err;
-                // console.log('Department are: ')
-                // console.table(output);
-                mainPrompt();
-            }
-            else if (ans.firstPrompt === 'view all roles') {
-                console.log('You choose ' + ans.firstPrompt)
-                mainPrompt();
-            }
+        // if(err) throw err;
+        // console.log('Department are: ')
+        // console.table(output);
+        await mainPrompt();
+    }
+    else if (ans.firstPrompt === 'view all roles') {
+        console.log('You choose ' + ans.firstPrompt)
+        await mainPrompt();
+    }
 
-            else if (ans.firstPrompt === 'view all employees') {
+    else if (ans.firstPrompt === 'view all employees') {
 
-                mainPrompt();
-            }
+        await mainPrompt();
+    }
 
-            else if (ans.firstPrompt === 'add a department') {
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'inputDepartment',
-                    message: 'Input the name of department'
-                }]).then(() => mainPrompt())
-            }
+    else if (ans.firstPrompt === 'add a department') {
+        await inquirer.prompt([{
+            type: 'input',
+            name: 'inputDepartment',
+            message: 'Input the name of department'
+        }])
+        await mainPrompt()
+    }
 
-            else if (ans.firstPrompt === 'add a role') {
+    else if (ans.firstPrompt === 'add a role') {
 
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'inputRole',
-                    message: 'Input the name of role'
-                }]).then(() => mainPrompt())
-            }
+        await inquirer.prompt([{
+            type: 'input',
+            name: 'inputRole',
+            message: 'Input the name of role'
+        }])
+        await mainPrompt()
+    }
 
-            else if (ans.firstPrompt === 'add an employee') {
+    else if (ans.firstPrompt === 'add an employee') {
 
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'inputEmployee',
-                    message: 'Input the name of employee'
-                }]).then(() => mainPrompt())
-            }
+        await inquirer.prompt([{
+            type: 'input',
+            name: 'inputEmployee',
+            message: 'Input the name of employee'
+        }])
+        await mainPrompt()
+    }
 
-            else {
+    else {
 
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'updateRole',
-                    message: 'select the employee to update'
-                }]).then(() => mainPrompt())
-            }
-        })
+        await inquirer.prompt([{
+            type: 'input',
+            name: 'updateRole',
+            message: 'select the employee to update'
+        }])
+        await mainPrompt()
+    }
 }
-//     })
-// }
 
 mainPrompt();
